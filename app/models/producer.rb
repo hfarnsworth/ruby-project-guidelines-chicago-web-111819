b95e852b1de83e2143ba6ac71484ff01cbe4b5dc
dut-a <d.dut@live.com>
2019-12-05 12:14:49 -0600

    Added base producer method

--- app/models/producer.rb
+++ app/models/producer.rb
@@ -2,6 +2,16 @@ class Producer < ActiveRecord::Base
     belongs_to :user
     has_many :scripts
 
-    # Script.first.update(producer_id: p1.id)
-    # Script.first.update(greenlit: true)
+    # takes script object and attaches self.id to producer_id value,
+    # also switches greenlit value to 'true'
+    def buy_script(script)
+        target_script = Script.find(script.id)
+        if target_script && (target_script.greenlit == false || target_script.greenlit == nil)
+            target_script.update(producer_id: self.id, greenlit: true)
+            return "Script successfully bought!"
+        else
+            return "Oops, you can't buy this script at this time :("
+        end
+    end
+
 end
