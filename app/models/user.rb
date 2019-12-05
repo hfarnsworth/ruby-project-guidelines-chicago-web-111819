c7460c951c642cb97ccc5d0801ffefee7c97fd8a
dut-a <d.dut@live.com>
2019-12-05 10:34:56 -0600

    Completed base user methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,20 @@
 class User < ActiveRecord::Base
     has_one :writer
     has_one :producer
+
+    # accepts name argument in order to find Object for the entered argument returns nil if not found
+    def self.get_user(user_name)
+        self.find_by(name: user_name)
+    end
+
+    # acts on self to create new writer object linked to self.id
+    def new_writer
+        Writer.create(user_id: self.id)
+    end
+
+    # acts on self to create new producer object linked to self.id
+    def new_producer
+        Producer.create(user_id: self.id)
+    end
+
 end
